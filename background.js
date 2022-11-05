3e7bc27f6c5cb2340fa3455492f66e62b1eb6cc1
hdboye <tariq2007nl@gmail.com>
2022-11-06 00:55:51 +0100

    now supports both the store and community pages + allows for enabling one and disabling the other

--- background.js
+++ background.js
@@ -2,9 +2,18 @@ chrome.webNavigation.onCommitted.addListener(function(e){redirect(e)}, {url: [{h
 chrome.webNavigation.onCommitted.addListener(function(e){redirect(e)}, {url: [{hostSuffix: 'store.steampowered.com'}]});
 
 function redirect(e){
-  chrome.storage.sync.get(['enabled'], function(res){
-    if(res.enabled){
-      chrome.tabs.update({url:"steam://openurl/"+e.url});
-    }
-  });
+  if(e.url.startsWith('https://steamcommunity.com')){
+    chrome.storage.sync.get(['enabled'], function(res){
+      if(res.enabled){
+        chrome.tabs.update({url:"steam://openurl/"+e.url});
+      }
+    });
+  }
+  if(e.url.startsWith('https://store.steampowered.com')){
+    chrome.storage.sync.get(['enabledt'], function(res){
+      if(res.enabledt){
+        chrome.tabs.update({url:"steam://openurl/"+e.url});
+      }
+    });
+  }  
 }
--- popup.js
+++ popup.js
@@ -6,12 +6,18 @@ chrome.windows.getLastFocused(
     }
 );
 function init(){
-    document.getElementById('enabled').checked
     chrome.storage.sync.get(['enabled'],function(res){
         document.getElementById('enabled').checked = res.enabled;
     });
     document.getElementById('enabled').addEventListener('change', (event) => {
         console.log("yes");
         chrome.storage.sync.set({enabled: event.currentTarget.checked},function(){return;});
+    })
+
+    chrome.storage.sync.get(['enabledt'],function(res){
+        document.getElementById('enabled2').checked = res.enabledt;
+    });
+    document.getElementById('enabled2').addEventListener('change', (event) => {
+        chrome.storage.sync.set({enabledt: event.currentTarget.checked},function(){return;});
     })
 };
