4dbd3cce768c4b48cb2630b9a6a48ef44c801ac7
hdboye <tariq2007nl@gmail.com>
2022-11-06 00:34:01 +0100

    HOW DID I JUST NOTICE I FORGOT STORE.STEAMPOWERED.COM

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
-chrome.webNavigation.onCommitted.addListener(function(e) {
+chrome.webNavigation.onCommitted.addListener(redirect(e), {url: [{hostSuffix: 'steamcommunity.com'}]});
+chrome.webNavigation.onCommitted.addListener(redirect(e), {url: [{hostSuffix: 'store.steampowered.com'}]});
+
+function redirect(e){
   chrome.storage.sync.get(['enabled'], function(res){
     if(res.enabled){
       chrome.tabs.update({url:"steam://openurl/"+e.url});
     }
   });
-}, {url: [{hostSuffix: ['steamcommunity.com','store.steampowered.com']}]});
+}
