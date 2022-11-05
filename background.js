2667edb6611ee8ef54e31a54eba4670b19610447
hdboye <tariq2007nl@gmail.com>
2022-11-06 00:31:23 +0100

    oops

--- background.js
+++ background.js
@@ -4,4 +4,4 @@ chrome.webNavigation.onCommitted.addListener(function(e) {
       chrome.tabs.update({url:"steam://openurl/"+e.url});
     }
   });
-}, {url: [{hostSuffix: 'steamcommunity.com'}]});
+}, {url: [{hostSuffix: ['steamcommunity.com','store.steampowered.com']}]});
