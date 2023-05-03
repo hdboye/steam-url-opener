7e3d62b823b2b35992f0574e9aa599e80bca824f
hdboye <tariq2007nl@gmail.com>
2023-05-03 02:54:11 +0200

    im os fucking stupid

--- background.js
+++ background.js
@@ -32,14 +32,16 @@ function redirect(e){
   if(e.url.startsWith('https://store.steampowered.com') && !(e.url.startsWith('https://store.steampowered.com/widget/'))){
     chrome.storage.sync.get(['enabledt'], function(res){
       if(res.enabledt){
-        chrome.tabs.update({url:"steam://openurl/"+e.url}).then(()=>{
-          setTimeout(()=>{
-            chrome.storage.sync.get(['closepages'], function(res2){
-              if(res2.closepages){
-                chrome.tabs.remove(e.tabId);
-              };
+        chrome.storage.sync.get(['askfirst'], function(res2){
+          if(!res2.askfirst){
+            chrome.tabs.update({url:"steam://openurl/"+e.url}).then(()=>{
+              doDat(e);
             });
-          }, 1000)
+          }else{
+            chrome.tabs.create({url:"steam://openurl/"+e.url}).then(()=>{
+              doDat(e);
+            });
+          }
         });
       }
     });
@@ -47,14 +49,16 @@ function redirect(e){
   if(e.url.startsWith('https://help.steampowered.com')){
     chrome.storage.sync.get(['enabledh'], function(res){
       if(res.enabledh){
-        chrome.tabs.update({url:"steam://openurl/"+e.url}).then(()=>{
-          setTimeout(()=>{
-            chrome.storage.sync.get(['closepages'], function(res2){
-              if(res2.closepages){
-                chrome.tabs.remove(e.tabId);
-              };
+        chrome.storage.sync.get(['askfirst'], function(res2){
+          if(!res2.askfirst){
+            chrome.tabs.update({url:"steam://openurl/"+e.url}).then(()=>{
+              doDat(e);
             });
-          }, 1000)
+          }else{
+            chrome.tabs.create({url:"steam://openurl/"+e.url}).then(()=>{
+              doDat(e);
+            });
+          }
         });
       }
     });
