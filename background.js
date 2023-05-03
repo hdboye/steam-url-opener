9573d09ffe94ba2bd4253c86c1fb5165e164f3dd
hdboye <tariq2007nl@gmail.com>
2023-05-03 02:48:14 +0200

    they done updated this after half a year

--- background.js
+++ background.js
@@ -2,25 +2,60 @@ chrome.webNavigation.onCommitted.addListener(function(e){redirect(e)}, {url: [{h
 chrome.webNavigation.onCommitted.addListener(function(e){redirect(e)}, {url: [{hostSuffix: 'store.steampowered.com'}]});
 chrome.webNavigation.onCommitted.addListener(function(e){redirect(e)}, {url: [{hostSuffix: 'help.steampowered.com'}]});
 
+function doDat(e){
+  setTimeout(()=>{
+    chrome.storage.sync.get(['closepages'], function(res){
+      if(res.closepages){
+        chrome.tabs.remove(e.tabId);
+      };
+    });
+  }, 1000)
+}
 function redirect(e){
   if(e.url.startsWith('https://steamcommunity.com')){
     chrome.storage.sync.get(['enabled'], function(res){
       if(res.enabled){
-        chrome.tabs.update({url:"steam://openurl/"+e.url});
+        chrome.storage.sync.get(['askfirst'], function(res2){
+          if(!res2.askfirst){
+            chrome.tabs.update({url:"steam://openurl/"+e.url}).then(()=>{
+              doDat(e);
+            });
+          }else{
+            chrome.tabs.create({url:"steam://openurl/"+e.url}).then(()=>{
+              doDat(e);
+            });
+          }
+        });
       }
     });
   }
   if(e.url.startsWith('https://store.steampowered.com') && !(e.url.startsWith('https://store.steampowered.com/widget/'))){
     chrome.storage.sync.get(['enabledt'], function(res){
       if(res.enabledt){
-        chrome.tabs.update({url:"steam://openurl/"+e.url});
+        chrome.tabs.update({url:"steam://openurl/"+e.url}).then(()=>{
+          setTimeout(()=>{
+            chrome.storage.sync.get(['closepages'], function(res2){
+              if(res2.closepages){
+                chrome.tabs.remove(e.tabId);
+              };
+            });
+          }, 1000)
+        });
       }
     });
   }  
   if(e.url.startsWith('https://help.steampowered.com')){
     chrome.storage.sync.get(['enabledh'], function(res){
       if(res.enabledh){
-        chrome.tabs.update({url:"steam://openurl/"+e.url});
+        chrome.tabs.update({url:"steam://openurl/"+e.url}).then(()=>{
+          setTimeout(()=>{
+            chrome.storage.sync.get(['closepages'], function(res2){
+              if(res2.closepages){
+                chrome.tabs.remove(e.tabId);
+              };
+            });
+          }, 1000)
+        });
       }
     });
   }  
--- popup.js
+++ popup.js
@@ -12,19 +12,32 @@ function init(){
     document.getElementById('enabled').addEventListener('change', (event) => {
         console.log("yes");
         chrome.storage.sync.set({enabled: event.currentTarget.checked},function(){return;});
-    })
+    });
 
     chrome.storage.sync.get(['enabledt'],function(res){
         document.getElementById('enabled2').checked = res.enabledt;
     });
     document.getElementById('enabled2').addEventListener('change', (event) => {
         chrome.storage.sync.set({enabledt: event.currentTarget.checked},function(){return;});
-    })
+    });
 
     chrome.storage.sync.get(['enabledh'],function(res){
         document.getElementById('enabled3').checked = res.enabledh;
     });
     document.getElementById('enabled3').addEventListener('change', (event) => {
         chrome.storage.sync.set({enabledh: event.currentTarget.checked},function(){return;});
+    });
+
+    chrome.storage.sync.get(['closepages'],function(res){
+        document.getElementById('closepage').checked = res.closepages;
+    });
+    document.getElementById('closepage').addEventListener('change', (event) => {
+        chrome.storage.sync.set({closepages: event.currentTarget.checked},function(){return;});
+    })
+    chrome.storage.sync.get(['askfirst'],function(res){
+        document.getElementById('askfirst').checked = res.askfirst;
+    });
+    document.getElementById('askfirst').addEventListener('change', (event) => {
+        chrome.storage.sync.set({askfirst: event.currentTarget.checked},function(){return;});
     })
 };
